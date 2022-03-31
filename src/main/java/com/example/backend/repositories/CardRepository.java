5ddf39a40201a88674d1e0ab08710b228d570e1f
Thony <thon0101@stud.kea.dk>
2022-03-31 13:22:11 +0200

    featurebranch CardRepo

--- src/main/java/com/example/backend/repositories/CardRepository.java
+++ src/main/java/com/example/backend/repositories/CardRepository.java
@@ -1,6 +1,7 @@
 package com.example.backend.repositories;
 
+import com.example.backend.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CardRepository extends JpaRepository<> {
+public interface CardRepository extends JpaRepository<Card, String> {
 }
