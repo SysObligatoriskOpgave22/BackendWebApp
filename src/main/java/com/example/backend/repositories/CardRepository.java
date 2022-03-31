7b8122eb425f6b80d2e3ad9a4bdf05c18965d643
Thony <thon0101@stud.kea.dk>
2022-03-31 13:35:11 +0200

    featurebranch Entity Deck

--- src/main/java/com/example/backend/repositories/CardRepository.java
+++ src/main/java/com/example/backend/repositories/CardRepository.java
@@ -3,5 +3,5 @@ package com.example.backend.repositories;
 import com.example.backend.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CardRepository extends JpaRepository<Card, String> {
+public interface CardRepository extends JpaRepository<Card, Long> {
 }
