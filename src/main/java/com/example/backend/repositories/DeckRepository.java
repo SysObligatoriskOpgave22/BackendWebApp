ca62e63a22f0e08e81aaad93809427ccbfe32473
Simon Gredal <simon.gredal@icloud.com>
2022-04-05 10:14:04 +0200

    Create Repository interface files

--- src/main/java/com/example/backend/repositories/DeckRepository.java
+++ src/main/java/com/example/backend/repositories/DeckRepository.java
@@ -1,9 +1,9 @@
 package com.example.backend.repositories;
 
-import com.example.backend.entity.Card;
+import com.example.backend.entity.Deck;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CardRepository extends JpaRepository<Card, Long> {
+public interface DeckRepository extends JpaRepository<Deck, Long> {
 }
