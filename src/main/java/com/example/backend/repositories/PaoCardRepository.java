30461c1512e7ee547d32425ab8f61e1c00ef56dd
Simon Gredal <simon.gredal@icloud.com>
2022-04-05 10:29:33 +0200

    Fix stupid typo

--- src/main/java/com/example/backend/repositories/PaoCardRepository.java
+++ src/main/java/com/example/backend/repositories/PaoCardRepository.java
@@ -1,8 +1,9 @@
 package com.example.backend.repositories;
 
+import com.example.backend.entity.PaoCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaoCardRepository extends JpaRepository<PaoCardRepository, Long> {
+public interface PaoCardRepository extends JpaRepository<PaoCard, Long> {
 }
