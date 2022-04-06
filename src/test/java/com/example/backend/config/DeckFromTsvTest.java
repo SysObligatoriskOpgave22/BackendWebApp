26e50ffaf99b346a8bb593799cef71462adb87b6
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2022-04-06 23:11:55 +0200

    Signatur tilføjet

--- src/test/java/com/example/backend/config/DeckFromTsvTest.java
+++ src/test/java/com/example/backend/config/DeckFromTsvTest.java
@@ -9,11 +9,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+//ATL
 class DeckFromTsvTest {
 
 
