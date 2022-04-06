dad04ce59062ccee3d55c66a3cdfbb9db2aaf893
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2022-04-06 23:11:11 +0200

    Signatur tilføjet

--- src/test/java/com/example/backend/api/CardControllerTest.java
+++ src/test/java/com/example/backend/api/CardControllerTest.java
@@ -27,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+//ATL
 class CardControllerTest {
 
     @Autowired
